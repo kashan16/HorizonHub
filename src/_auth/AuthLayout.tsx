9a204acfd259a6a9505c3dba604ac8b42492dcdf
Mohd Kashan Yunus <mohdkashan21165@gmail.com>
2023-12-04 15:21:34 +0530

    Auth Pages

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -1,8 +1,25 @@
 /* Wraps both sign-in and sing-up function */
+import { Navigate, Outlet } from "react-router-dom";
 
 const AuthLayout = () => {
+
+  const isAuthenticated = false;
+
   return (
-    <div>AuthLayout</div>
+    <>
+      {isAuthenticated ? (
+        <Navigate to='/'/>
+      ) : (<>
+      <section className="flex flex-1 justify-center items-center py-10 flex-col">
+        <Outlet/>
+      </section>
+      <img 
+        src="/assets/1.png"
+        alt="logo"
+        className="hidden xl:block h-screen w-1/2 object-cover bg-no-repeat"
+        />
+      </>)}
+    </>
   )
 }
 
--- src/_auth/forms/SigninForm.tsx
+++ src/_auth/forms/SigninForm.tsx
@@ -1,7 +1,11 @@
-
+import { Button } from "@/components/ui/button"
 const SigninForm = () => {
   return (
-    <div>SigninForm</div>
+    <div>
+      <Button>
+        Click Here
+      </Button>
+    </div>
   )
 }
 
