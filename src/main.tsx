90b114f4ff0736ade1335ee11079e2e4f33c0261
Mohd Kashan Yunus <mohdkashan21165@gmail.com>
2023-12-12 20:29:03 +0530

    Fixed Database

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,20 @@
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App.tsx'
-import AuthProvider from './context/AuthContext.tsx'
-import './index.css'
-import QueryProvider from './lib/react-query/QueryProvider.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <QueryProvider>
-      <AuthProvider>
-        <App/>
-      </AuthProvider>
-    </QueryProvider>
-  </BrowserRouter>
-)
+import { AuthProvider } from "@/context/AuthContext";
+import { QueryProvider } from "@/lib/react-query/QueryProvider";
+
+import App from "./App";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <QueryProvider>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </QueryProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
