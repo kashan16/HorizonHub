03b7b72645ef7d1f779dfafcfc74e0ae6d4e20a4
Mohd Kashan Yunus <mohdkashan21165@gmail.com>
2023-12-12 20:26:47 +0530

    Code optimization

--- src/types/index.ts
+++ src/types/index.ts
@@ -46,12 +46,3 @@ export type INewUser = {
   username: string;
   password: string;
 };
-
-export type IContextType = {
-  user : IUser;
-  isLoading : boolean;
-  setUser : React.Dispatch<React.SetStateAction<IUser>>;
-  isAuthenticated : boolean;
-  setIsAuthenticated : React.Dispatch<React.SetStateAction<boolean>>;
-  checkAuthUser : () => Promise<boolean>;
-}
