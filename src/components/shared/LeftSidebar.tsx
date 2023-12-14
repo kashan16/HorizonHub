eb1c52289cee74cb462ab5eb6cc62ee8acb6e8b9
Mohd Kashan Yunus <mohdkashan21165@gmail.com>
2023-12-14 21:58:40 +0530

    Fixed Leftsidebar

--- src/components/shared/LeftSidebar.tsx
+++ src/components/shared/LeftSidebar.tsx
@@ -1,10 +1,14 @@
 import { useEffect } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, NavLink, useLocation, useNavigate } from "react-router-dom";
 
+import { sidebarLinks } from "@/constants";
 import { useUserContext } from "@/context/AuthContext";
 import { useSignOutAccount } from "@/lib/react-query/queries";
+import { INavLink } from "@/types/index";
+import { Button } from "../ui/button";
 
 const LeftSidebar = () => {
+  const { pathname } = useLocation();
   const navigate = useNavigate();
   const { user } = useUserContext();
   const { mutate: signOut, isSuccess } = useSignOutAccount();
@@ -38,7 +42,36 @@ const LeftSidebar = () => {
               </p>
             </div>
           </Link>
+          <ul className="flex flex-col gap-6">
+            {sidebarLinks.map((link : INavLink) => {
+              const isActive = pathname === link.route;
+              return (
+                <li key = {link.label} className={`leftsidebar-link group ${isActive && 'bg-primary-500'}`}>
+                  <NavLink 
+                  to = {link.route}
+                  className="flex gap-4 items-center p-4">
+                    <img 
+                      src={link.imgURL}
+                      alt={link.label}
+                      className={`group-hover:invert-white ${isActive && 'invert-white'}`}/>
+                    {
+                      link.label
+                    }
+                  </NavLink>
+                </li>
+              )
+            })}
+          </ul>
       </div>
+      <Button
+            variant="ghost"
+            className="shad-button_ghost"
+            onClick={() => signOut()}>
+            <img src="/assets/icons/logout.svg" alt="logout" />
+            <p className="small-medium lg:base-medium">
+              Logout
+            </p>
+      </Button>
     </div>
   )
 }
