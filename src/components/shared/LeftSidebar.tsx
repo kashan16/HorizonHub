dd2be0a442e61c8a69f8da6930111d434157c01f
Mohd Kashan Yunus <mohdkashan21165@gmail.com>
2023-12-14 21:38:43 +0530

    Created Sidebar

--- src/components/shared/LeftSidebar.tsx
+++ src/components/shared/LeftSidebar.tsx
@@ -1,7 +1,45 @@
+import { useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+import { useUserContext } from "@/context/AuthContext";
+import { useSignOutAccount } from "@/lib/react-query/queries";
 
 const LeftSidebar = () => {
+  const navigate = useNavigate();
+  const { user } = useUserContext();
+  const { mutate: signOut, isSuccess } = useSignOutAccount();
+
+  useEffect(() => {
+    if (isSuccess) navigate(0);
+  }, [isSuccess , navigate]);
   return (
-    <div>LeftSidebar</div>
+    <div className="leftsidebar">
+      <div className="flex flex-col gap-11">
+      <Link to="/" className="flex gap-3 items-center">
+          <img
+            src="/assets/HH_LOGO1.png"
+            alt="logo"
+            width={170}
+            height={36}
+          />
+        </Link>
+        <Link to={`/profile/${user.id}`} className="flex gap-3 items-center">
+            <img
+              src={user.imageUrl || "/assets/icons/profile-placeholder.svg"}
+              alt="profile"
+              className="h-14 w-14 rounded-full"
+            />
+            <div className="flex flex-col">
+              <p className="body-bold">
+                {user.name}
+              </p>
+              <p className="small-regular text-light-3">
+                @{user.username}
+              </p>
+            </div>
+          </Link>
+      </div>
+    </div>
   )
 }
 
