a89f3b44f46979f92e66174ac8c7d8552ffda966
Mohd Kashan Yunus <mohdkashan21165@gmail.com>
2023-12-14 21:10:12 +0530

    Created Topbar

--- src/components/shared/Topbar.tsx
+++ src/components/shared/Topbar.tsx
@@ -1,7 +1,18 @@
+import { Link } from "react-router-dom"
 
 const Topbar = () => {
   return (
-    <div>Topbar</div>
+    <section className="topbar" >
+      <div className="flex-between py-4 px-5">
+        <Link to = "/" className="flex gap-3 items-center">
+          <img 
+            src="/assets/HH_LOGO1.png"
+            alt="logo"
+            width={130}
+            height={325}/>
+        </Link>
+      </div>
+    </section>
   )
 }
 
