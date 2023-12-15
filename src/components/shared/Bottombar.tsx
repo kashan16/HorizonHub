1a1654d93881f965a7c65837ec5bbabf6b0b2651
Mohd Kashan Yunus <mohdkashan21165@gmail.com>
2023-12-15 14:40:10 +0530

    Fixed Bottombar for mobile view

--- src/components/shared/Bottombar.tsx
+++ src/components/shared/Bottombar.tsx
@@ -1,7 +1,28 @@
+import { bottombarLinks } from "@/constants";
+import { INavLink } from "@/types/index";
+import { NavLink, useLocation } from "react-router-dom";
 
 const Bottombar = () => {
+  const { pathname } = useLocation();
   return (
-    <div>Bottombar</div>
+    <section className="bottom-bar">
+      {bottombarLinks.map((link : INavLink) => {
+              const isActive = pathname === link.route;
+              return (
+                  <NavLink 
+                  to = {link.route}
+                  key = {link.label} className={`${isActive && 'bg-primary-500 rounded-[10px]'} flex-center flex-col gap-1 p-2 transition rounded-md`}>
+                    <img 
+                      src={link.imgURL}
+                      alt={link.label}
+                      width={20}
+                      height={20}
+                      className={`${isActive && 'invert-white'}`}/>
+                      <p className="tiny-medium text-light-2">{link.label}</p>
+                  </NavLink>
+              )
+            })}
+    </section>
   )
 }
 
