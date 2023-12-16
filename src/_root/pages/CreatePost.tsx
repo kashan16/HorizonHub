96d06e7f58f7243e33f14307da0290476781b2f2
Mohd Kashan Yunus <mohdkashan21165@gmail.com>
2023-12-16 16:32:30 +0530

    Created Post functionality

--- src/_root/pages/CreatePost.tsx
+++ src/_root/pages/CreatePost.tsx
@@ -1,7 +1,20 @@
 
 const CreatePost = () => {
   return (
-    <div>CreatePost</div>
+    <div className="flex flex-1">
+      <div className="common-container">
+        <div className="max-w-5xl flex-start gap-3 justify-start">
+          <img
+            src = "/assets/icons/add-post.svg"
+            width={36}
+            height={36}
+            alt = "add"/>
+            <h2 className="h3-bold md:h2-bold text-left w-full">
+              Post
+            </h2>
+        </div>
+      </div>
+    </div>
   )
 }
 
